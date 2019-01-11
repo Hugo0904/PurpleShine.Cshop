64650a1b150a466ef7c933f8abc12e10b6c06d9c
falseapple <derlerfairyland@gmail.com>
2019-01-12 00:09:43 +0800

    優化HTTP

--- PurpleShine.Net/Http/HttpClientPool.cs
+++ PurpleShine.Net/Http/HttpClientPool.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -10,16 +9,6 @@ namespace PurpleShine.Net.Http
 {
     public static class HttpClientPool
     {
-        public static Dictionary<string, string> DEFAULT_HEADER = new Dictionary<string, string>()
-        {
-            { "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" },
-            { "Accept-Encoding", "gzip, deflate, sdch" },
-            { "Accept-Language", "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4" },
-            { "Connection", "keep-alive" },
-            { "KeepAlive", "false" },
-            { "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" }
-        };
-
         /// <summary>
         /// Create HttpClient
         /// </summary>
---
+++ PurpleShine.Net/Http/HttpHeaders.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PurpleShine.Net.Http
+{
+    public static class HttpHeaders
+    {
+        public static Dictionary<string, string> DEFAULT_HEADER = new Dictionary<string, string>()
+        {
+            { "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" },
+            { "Accept-Encoding", "gzip, deflate, sdch" },
+            { "Accept-Language", "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4" },
+            { "Connection", "keep-alive" },
+            { "KeepAlive", "false" },
+            { "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" }
+        };
+    }
+}
