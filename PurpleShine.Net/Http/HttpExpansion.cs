e435d69215e6fbebbd25ce9d59e8c38635ef9ee5
hugo <hugo@codegod.com.net>
2020-09-20 11:58:15 +0800

    更新

--- PurpleShine.Net/Http/HttpExpansion.cs
+++ PurpleShine.Net/Http/HttpExpansion.cs
@@ -8,6 +8,9 @@ using System.Web;
 
 namespace PurpleShine.Net.Http
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class HttpExpansion
     {
         /// <summary>
@@ -16,6 +19,7 @@ namespace PurpleShine.Net.Http
         /// <param name="client"></param>
         /// <param name="path"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendGetAsync(this HttpClient client, string path, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -54,6 +58,7 @@ namespace PurpleShine.Net.Http
         /// <param name="client"></param>
         /// <param name="path"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendGetAsync<T>(this HttpClient client, string path, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null) where T : class
         {
@@ -92,6 +97,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnRequestContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendPostAsync(this HttpClient client, string url, Dictionary<string, string> data, int timeout = Timeout.Infinite, Action<HttpContent> OnRequestContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -134,6 +141,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="content"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnRequestContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendPostAsync(this HttpClient client, string url, HttpContent content, int timeout = Timeout.Infinite, Action<HttpContent> OnRequestContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -175,6 +184,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="content"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnRequestContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendPostAsync<T>(this HttpClient client, string url, HttpContent content, int timeout = Timeout.Infinite, Action<HttpContent> OnRequestContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -215,6 +226,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnRequestContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendPostAsync<T>(this HttpClient client, string url, Dictionary<string, string> data, int timeout = Timeout.Infinite, Action<HttpContent> OnRequestContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -256,6 +269,7 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendPostAsJsonAsync(this HttpClient client, string url, object data, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -294,6 +308,7 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendPostAsJsonAsync<T>(this HttpClient client, string url, object data, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -332,6 +347,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendPutAsync(this HttpClient client, string url, Dictionary<string, string> data, int timeout = Timeout.Infinite, Action<HttpContent> OnContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -373,6 +390,8 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnContent"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendPutAsync<T>(this HttpClient client, string url, Dictionary<string, string> data, int timeout = Timeout.Infinite, Action<HttpContent> OnContent = null, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -414,6 +433,7 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendPutAsJsonAsync(this HttpClient client, string url, object data, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -452,6 +472,7 @@ namespace PurpleShine.Net.Http
         /// <param name="url"></param>
         /// <param name="data"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendPutAsJsonAsync<T>(this HttpClient client, string url, object data, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -489,6 +510,7 @@ namespace PurpleShine.Net.Http
         /// <param name="client"></param>
         /// <param name="path"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<string>> SendDeleteAsync(this HttpClient client, string path, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -526,6 +548,7 @@ namespace PurpleShine.Net.Http
         /// <param name="client"></param>
         /// <param name="path"></param>
         /// <param name="timeout"></param>
+        /// <param name="OnResponse"></param>
         /// <returns></returns>
         public static async Task<HttpResponse<T>> SendDeleteAsync<T>(this HttpClient client, string path, int timeout = Timeout.Infinite, Action<HttpResponseMessage> OnResponse = null)
         {
@@ -557,6 +580,11 @@ namespace PurpleShine.Net.Http
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
         public static Dictionary<string, string> ConvertGetQueryToDictionary(this string @this)
         {
             if (@this.StartsWith("&"))
